fad94e12b724e9a96966088796d862cba08c502e
Vivek Nair <vnair611@stanford.edu>
2012-02-11 15:10:49 -0800

    First Commit with Models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Endowr::Application.config.secret_token = '66aa2718f49451b9fdffcab982fc53d660fa79cea62bdb2bd9fed12985f50560b337b0e69b90b4123c76730e426f5c731097aca46b6344174cb645a32e9fa2c8'
